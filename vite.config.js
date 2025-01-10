319a40297fbf03c4b0f2f3ead540e5242fdb301a
JazzyRain14 <ajadiolamilekan14@gmail.com>
2025-01-10 02:56:04 +0100

    tampered with the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
+  },
+  server: {
+    open: true,
+  },
+  // Add this block for SPA fallback
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/',
 })
